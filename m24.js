180dcee599ad4787d83130b0906707f546524db6
RahimAbbas55 <irahimsindhu@gmail.com>
2026-01-07 16:02:09 +0500

    Group Anagrams - (Leetcode Medium Difficulty Problem 49)

---
+++ m24.js
@@ -0,0 +1,46 @@
+/*
+    49. Group Anagrams
+    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    Example 1:
+        Input: strs = ["eat","tea","tan","ate","nat","bat"]
+        Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        Explanation:
+        There is no string in strs that can be rearranged to form "bat".
+        The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+        The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+    Example 2:
+        Input: strs = [""]
+        Output: [[""]]
+    Example 3:
+        Input: strs = ["a"]
+        Output: [["a"]]
+    Constraints:
+        1 <= strs.length <= 104
+        0 <= strs[i].length <= 100
+        strs[i] consists of lowercase English letters.
+*//**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+    for (let str of strs) {
+        // frequency array for 26 lowercase letters
+        const count = Array(26).fill(0);
+
+        for (let char of str) {
+            count[char.charCodeAt(0) - 97]++;
+        }
+
+        // convert array to string to use as map key
+        const key = count.join('#');
+
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(str);
+    }
+
+    return Array.from(map.values());
+};
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
